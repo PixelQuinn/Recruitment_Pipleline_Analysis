d0aa9fe1f5da816cbcf2ed34e401a95b192d14ee
Quinn Reams <quinn.reams@icloud.com>
2025-01-23 15:44:50 -0500

    script: extract average salary by category

---
+++ scripts/Salary_Analysis.py
@@ -0,0 +1,18 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path = ("data/raw/recruitment_data.csv")
+data = pd.read_csv(file_path)
+
+# Average salary by education level
+avg_salary_by_edlevel = data.groupby('EdLevel')['PreviousSalary'].mean()
+
+# Average salary by gender
+avg_salary_by_gender = data.groupby('Gender')['PreviousSalary'].mean()
+
+# Save to CSV
+avg_salary_by_edlevel.to_csv('data/processed/Salary_By_EdLevel.csv', index=True)
+avg_salary_by_gender.to_csv('data/processed/Salary_By_Gender.csv', index=True)
